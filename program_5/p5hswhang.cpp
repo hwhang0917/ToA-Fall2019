d440cebf8bf212dc279077831c417364a9ca93b7
Mark Whang <hwhang0917@gmail.com>
2019-11-26 08:01:32 -0800

    Initiate program 5

--- program_5/p5hswhang.cpp
+++ program_5/p5hswhang.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <iostream> // std::cout, std::cin
+#include <algorithm> // std::min
+#include <string> // std::string
+#include <vector> // std::vector
 
 struct matrix_dim { // Matrix dimension
     int row;
     int column;
 };
 
+// Utility Function
 bool boundError(int matrix_num) {
     // the number of matrix should be <10 or >0
     if (matrix_num > 10) {
@@ -18,6 +20,22 @@ bool boundError(int matrix_num) {
     } else {
         return false;
     }
+}
+
+// Chain Multiplication Function
+int minmult (int n, const std::vector<int> D, std::vector<std::vector<int>>* M, std::vector<std::vector<int>>* P) {
+    int i, j, k, diagonal;
+
+    for (i = 0; i <= n; i++) // All the diagonal of matrix M costs 0 because Ai~Ai is no multiplication
+        (*M)[i][i] = 0;
+
+    for (diagonal = 1; diagonal <= n - 1; diagonal ++) {
+        for (i = 0; i <= n - diagonal; i++) {
+            j = i + diagonal;
+            (*M)[i][j] = std::min((*M)[i][k] + (*M)[k+1][j] + D[i-1] * D[k] * D[j]);
+            (*P)[i][j] = k;
+        }
+    }    
 }
 
 int main() {
